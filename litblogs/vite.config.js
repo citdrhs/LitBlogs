05bb81e8877f352cd2859f992a63d1ae60b529bc
Anthony Cavero <anthonyj.cavero@gmail.com>
2026-02-22 23:47:15 -0500

    Update API proxy URL in Vite config

--- litblogs/vite.config.js
+++ litblogs/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dren/'
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',
+      '/api': 'https://drhscit.org/dren',
     },
   },
 })
